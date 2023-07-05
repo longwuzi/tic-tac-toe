7e21149aa7071bb6caa70dfc0fb2c851a5cbebd4
Kohei Todoroki <m222408@hiroshima-u.ac.jp>
2023-07-05 01:21:29 +0000

    add 'o' test

--- tic-tac-toe_test.go
+++ tic-tac-toe_test.go
@@ -11,3 +11,13 @@ func TestPutToken01(t *testing.T) {
 		t.Errorf("Test fail expected: %s, result: %s\n", "o", b.get(1, 1))
 	}
 }
+
+func TestPutToken02(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(1, 1, "x")
+	if b.get(1, 1) != "x" {
+		t.Errorf("Test fail expected: %s, result: %s\n", "x", b.get(1, 1))
+	}
+}
