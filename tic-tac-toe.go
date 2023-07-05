44ff7402575e02ce9acb15b908126dbde4eb8bf8
Kohei Todoroki <m222408@hiroshima-u.ac.jp>
2023-07-05 01:53:59 +0000

    add judge

--- tic-tac-toe.go
+++ tic-tac-toe.go
@@ -7,13 +7,29 @@ type Board struct {
 func (b *Board) put(x, y int, u string) {
 	if u == "o" {
 		b.tokens[x+3*y] = 1
+	} else if u == "x" {
+		b.tokens[x+3*y] = -1
 	}
 }
+
 func (b *Board) get(x, y int) string {
 	if b.tokens[x+3*y] == 1 {
 		return "o"
+	} else if b.tokens[x+3*y] == -1 {
+		return "x"
 	}
-	return "x"
+	return ""
+}
+
+func (b *Board) judge() bool {
+	// 縦列判定
+	for i := 0; i < 3; i++ {
+		sum := b.tokens[i] + b.tokens[i+3] + b.tokens[i+6]
+		if sum == 3 || sum == -3 {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
--- tic-tac-toe_test.go
+++ tic-tac-toe_test.go
@@ -21,3 +21,21 @@ func TestPutToken02(t *testing.T) {
 		t.Errorf("Test fail expected: %s, result: %s\n", "x", b.get(1, 1))
 	}
 }
+
+func TestWinLose01(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	// 勝負がついていない状態での判定
+	if b.judge() {
+		t.Errorf("Test fail expected: %t, result: %t\n", false, b.judge())
+	}
+
+	// 縦列で勝負がついた状態での判定
+	b.put(0, 0, "o")
+	b.put(0, 1, "o")
+	b.put(0, 2, "o")
+	if !b.judge() {
+		t.Errorf("Test fail expected: %t, result: %t\n", true, b.judge())
+	}
+}
